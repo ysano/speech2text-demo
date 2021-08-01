3c60657af598a6ec591581a9af804b306f6b76b1
ysano <ysano@ysnet.org>
2021-08-01 16:00:48 +0900

    feat: search words

--- speech2text.go
+++ speech2text.go
@@ -9,24 +9,37 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	speech "cloud.google.com/go/speech/apiv1"
 	speechpb "google.golang.org/genproto/googleapis/cloud/speech/v1"
 )
 
+const (
+	// ANSI Color
+	colorRed   = "\033[0;31m"
+	colorGreen = "\033[0;32m"
+	colorBlue  = "\033[0;34m"
+	colorNone  = "\033[0m"
+)
+
 func main() {
 	// Usage
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s <audiofile>\n", filepath.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, "Usage: %s <audiofile> [[word1] [word2]...]\n", filepath.Base(os.Args[0]))
 		fmt.Fprintf(os.Stderr, "<audiofile>: 16khz 16bit little endian only")
+		fmt.Fprintf(os.Stderr, "[word1 word2..]: Search word(s)")
 	}
 
 	// Option Parse
 	flag.Parse()
-	if len(flag.Args()) != 1 {
+	if len(flag.Args()) == 0 {
 		log.Fatal("Specify a local audiofile.")
 	}
-	audioFile := flag.Arg(0)
+	args := flag.Args()
+	audioFile := args[0]
+	words := args[1:]
 
 	// Context
 	ctx := context.Background()
@@ -47,7 +60,27 @@ func main() {
 
 	// Get Transcript
 	transcript := getTrans(stream, audioFile)
-	fmt.Fprintf(os.Stdout, "%v\n", transcript)
+
+	// Output entire transcript
+	if len(words) == 0 {
+		fmt.Printf("%sSearch word is nil, output entire transcript%s\n", colorGreen, colorNone)
+		fmt.Printf("%v\n", transcript)
+		return
+	}
+
+	// Default word neighbors
+	neighbors := 5
+
+	// Output result per word
+	for _, w := range words {
+		results := parseSingle(transcript, w, neighbors)
+		results = formatString(results, w)
+		fmt.Printf("%sWord: %v%s\n", colorGreen, w, colorNone)
+		for i, v := range results {
+			fmt.Printf("%s%0d:%s %v\n", colorGreen, i, colorNone, v[0])
+		}
+	}
+	return
 }
 
 func getTrans(stream speechpb.Speech_StreamingRecognizeClient, audioFile string) string {
@@ -129,4 +162,37 @@ func getTrans(stream speechpb.Speech_StreamingRecognizeClient, audioFile string)
 		}
 	}
 	return ""
+}
+
+// Full-text search with a single target word
+func parseSingle(str string, target string, neighbors int) [][]string {
+
+	// Add dummy space for edge case
+	dummy := strings.Repeat(` `, neighbors)
+	str = dummy + str + dummy
+
+	// Find Target
+	rWithNeighbors := regexp.MustCompile(fmt.Sprintf(`.{%d}%v.{%d}`, neighbors, target, neighbors))
+	ret := rWithNeighbors.FindAllStringSubmatch(str, -1)
+
+	// Remove dummy
+	rDummy := regexp.MustCompile(`^\s+|\s+$`)
+	for _, v := range ret {
+		v[0] = rDummy.ReplaceAllString(v[0], "")
+		// log.Printf(v[0])
+	}
+
+	return ret
+}
+
+// ANSI color
+func formatString(results [][]string, target string) [][]string {
+
+	rTarget := regexp.MustCompile(target)
+
+	for _, v := range results {
+		v[0] = rTarget.ReplaceAllString(v[0], colorRed+"$0"+colorNone)
+		// log.Printf(v[0])
+	}
+	return results
 }
